9b5dccbd8bf776c694d0319795c638760fb81891
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2023-10-17 12:20:33 +0300

    Fixed #5 - Add animate directive

--- src/runtime/core/directives/index.ts
+++ src/runtime/core/directives/index.ts
@@ -3,5 +3,6 @@ export const directives = [
   { name: 'tooltip', as: 'Tooltip', from: 'primevue/tooltip' },
   { name: 'ripple', as: 'Ripple', from: 'primevue/ripple' },
   { name: 'styleclass', as: 'StyleClass', from: 'primevue/styleclass' },
-  { name: 'focustrap', as: 'FocusTrap', from: 'primevue/focustrap' }
+  { name: 'focustrap', as: 'FocusTrap', from: 'primevue/focustrap' },
+  { name: 'animate', as: 'Animate', from: 'primevue/animate' }
 ];
